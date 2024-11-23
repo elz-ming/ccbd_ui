262afd9278e1c889e47f5e9d8999d9fd7258b030
tehhanyi <hanyi.teh@beveat.com>
2024-11-23 14:51:21 +0800

    task6 using pretrained bert model instead

--- api/task6.py
+++ api/task6.py
@@ -7,12 +7,11 @@ from transformers import BertTokenizer, BertForSequenceClassification
 client = tweepy.Client(bearer_token=secret.BEARER_TOKEN)  # requires secret.py, which is not included in the repository cause idw my keys to be exposed. ask from han yi
 
 # Load pre-trained model and tokenizer
-# REQUIRES TO FINE TUNE MODEL FIRST! this is done at task6-bert.py so pls run "python api/task6-bert.py" first
-tokenizer = BertTokenizer.from_pretrained('./fine-tuned-bert')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')#./fine-tuned-bert')# REQUIRES TO FINE TUNE MODEL FIRST! this is done at task6-bert.py so pls run "python api/task6-bert.py" first
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
 
 # Load pre-trained sentiment analysis pipeline
-sentiment_pipeline = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+sentiment_pipeline = pipeline('sentiment-analysis', model=model,tokenizer=tokenizer)
 
 def fetch_tweets(query, max_results=10, tweet_fields=['text', 'lang']):
     while True:
@@ -24,33 +23,33 @@ def fetch_tweets(query, max_results=10, tweet_fields=['text', 'lang']):
             return []
             #time.sleep(15 * 60)  # Wait for 15 minutes before retrying
 
-def analyze_sentiment(tweets, collection):
+def analyze_sentiment(tweets):
     sentiments = []
     for tweet in tweets:
         result = sentiment_pipeline(tweet.text)
-        sentiments.append(result[0])
-    
-    if sentiments != []:
-        store_tweets(tweets, sentiments, collection)
-    
-    return sentiments
-
-def store_tweets(tweets, sentiments, collection):
-    for tweet, sentiment in zip(tweets, sentiments):
         tweet_data = {
             'text': tweet.text,
             'lang': tweet.lang,
-            'sentiment': sentiment
+            'sentiment': result[0]
         }
-        collection.insert_one(tweet_data)
+        sentiments.append(tweet_data)
+        
+    return sentiments
+
+def store_tweets(sentiments, collection):
+    for sentiment_data in sentiments:
+        collection.insert_one(sentiment_data)
     print("Tweets and sentiments stored in MongoDB.")
 
 def section6_data(collection):
     query = 'flight delay'
     tweets = fetch_tweets(query=query, max_results=10, tweet_fields=['text', 'lang'])
     tweet_texts = [tweet for tweet in tweets if tweet.lang == 'en']
-    sentiments = analyze_sentiment(tweet_texts, collection)
+    sentiments = analyze_sentiment(tweet_texts)
+    #     store_tweets(sentiments, collection)
 
-    """Query MongoDB to retrieve data for section 6."""
-    documents = collection.find({"query": query})
-    return list(documents)
+    #Query MongoDB to retrieve data for section 6
+    # documents = collection.find()
+    # print(documents)
+
+    return sentiments
