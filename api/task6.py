664f0c398f4435adef2f438be83b2d38e1b1a7ea
tehhanyi <hanyi.teh@beveat.com>
2024-11-23 15:17:38 +0800

    task 6 using 2 models to train bert

--- api/task6.py
+++ api/task6.py
@@ -1,17 +1,42 @@
 import tweepy
 import secret
+import numpy as np
+from scipy.special import softmax
 from transformers import pipeline
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoConfig, AutoTokenizer
+from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
 
 # Authenticate to Twitter using Twitter API v2
 client = tweepy.Client(bearer_token=secret.BEARER_TOKEN)  # requires secret.py, which is not included in the repository cause idw my keys to be exposed. ask from han yi
 
-# Load pre-trained model and tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')#./fine-tuned-bert')# REQUIRES TO FINE TUNE MODEL FIRST! this is done at task6-bert.py so pls run "python api/task6-bert.py" first
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+# Load DistilBERT model and tokenizer
+model_name_d = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer_d = AutoTokenizer.from_pretrained(model_name_d)
+config_d = AutoConfig.from_pretrained(model_name_d)
+model_d = AutoModelForSequenceClassification.from_pretrained(model_name_d)
+
+# Load BERT model and tokenizer
+model_name_b = 'bert-base-uncased'
+tokenizer_b = BertTokenizer.from_pretrained(model_name_b)
+config_b = BertConfig.from_pretrained(model_name_b)
+model_b = BertForSequenceClassification.from_pretrained(model_name_b)
+
+def sentiment_labels(text, model, tokenizer, config):
+    encoded_input = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    return config.id2label[ranking[0]]
 
 # Load pre-trained sentiment analysis pipeline
-sentiment_pipeline = pipeline('sentiment-analysis', model=model,tokenizer=tokenizer)
+# sentiment_pipeline = pipeline('sentiment-analysis', model=model,tokenizer=tokenizer)
 
 def fetch_tweets(query, max_results=10, tweet_fields=['text', 'lang']):
     while True:
@@ -26,11 +51,11 @@ def fetch_tweets(query, max_results=10, tweet_fields=['text', 'lang']):
 def analyze_sentiment(tweets):
     sentiments = []
     for tweet in tweets:
-        result = sentiment_pipeline(tweet.text)
+        result = sentiment_labels(tweet.text, model, tokenizer, config)
         tweet_data = {
             'text': tweet.text,
             'lang': tweet.lang,
-            'sentiment': result[0]
+            'sentiment': result
         }
         sentiments.append(tweet_data)
         
