cf83ba225813526db50657a511913fdf79d68351
Edmund Lin <72682413+elz-ming@users.noreply.github.com>
2024-11-25 15:26:25 +0800

    Edmund (#8)
    * display task1 data
    
    * test
    
    * test refactor
    
    * task 2 display
    
    * task 3 display
    
    * refactor task 1 - 4
    
    * refactor task6
    
    * test
    
    * improve ui up to task 5
    
    * Update styles.css
    
    * include 5a and 5b

--- modules/task5b.py
+++ modules/task5b.py
@@ -6,33 +6,21 @@ import pandas as pd
 
 matplotlib.use('Agg')  # Use a non-interactive backend
 
-def query_task5_data(collection):
+def query_task5b_data(collection):
     """Query MongoDB to retrieve data for task-5."""
-    document = collection.find_one({"file_name": "task5"})  # Query by file_name
+    document = collection.find_one({"file_name": "task5b"})  # Query by file_name
     if not document:
         return [], None  # Return empty list and None if document is not found
     return document.get("data", []), document.get("accuracy")  # Return data and accuracy
 
 
-def create_task5_table(data):
-    """Convert data for task-5 into an HTML table using Pandas."""
-    if not data:
-        return "<p>No data available for task-5.</p>"
-    
-    # Convert data to a Pandas DataFrame
-    df = pd.DataFrame(data)
-    
-    # Generate HTML table
-    return df.to_html(classes="table table-striped", index=False)
-
-
-def create_task5_chart(data):
+def create_task5b_chart(data):
     """Generate a bar chart for task-5 using Matplotlib."""
     if not data:
         return None
 
     # Extract sentiment distribution
-    sentiments = [item["calculated_sentiment"] for item in data]
+    sentiments = [item["airline_sentiment_gold"] for item in data]
     sentiment_counts = pd.Series(sentiments).value_counts()
 
     # Create the chart
